9c7139362885e2cdebc64ab2b947994d68465312
Jacob <londonvista@icloud.com>
2026-09-09 07:34:57 +0100

    Linux: Fix renderer click listener async syntax and bump version to 1.1.4

--- Linux/src/preload.js
+++ Linux/src/preload.js
@@ -1,7 +1,7 @@
 const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("bigu", {
-  version: "1.1.3",
+  version: "1.1.4",
   fetchAll: () => ipcRenderer.invoke("fetch-all"),
   fetchOne: (id) => ipcRenderer.invoke("fetch-one", id),
   login: (id) => ipcRenderer.invoke("login", id),

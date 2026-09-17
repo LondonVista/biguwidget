d4ec38010605325f39cad32fd72e5beb9da34023
LondonVista <noreply@github.com>
2026-09-17 21:56:43 +0000

    Windows: Bump version to v1.2.0 across manifests

--- Windows/src/preload.js
+++ Windows/src/preload.js
@@ -1,7 +1,7 @@
 const { contextBridge, ipcRenderer, webFrame } = require("electron");
 
 contextBridge.exposeInMainWorld("bigu", {
-  version: "1.1.9",
+  version: "1.2.0",
   fetchAll: () => ipcRenderer.invoke("fetch-all"),
   fetchOne: (id) => ipcRenderer.invoke("fetch-one", id),
   login: (id) => ipcRenderer.invoke("login", id),
